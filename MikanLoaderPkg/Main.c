62db4ebe5cfaf4cad4ada914edd35c3f42b7df7b
hide27k <hidehusky1996@gmail.com>
2021-03-25 17:01:01 -0700

    :recycle: Update screen color

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -225,21 +225,22 @@ EFI_STATUS EFIAPI UefiMain(
   if (EFI_ERROR(status)) {
     status = GetMemoryMap(&memmap);
     if (EFI_ERROR(status)) {
-      Print(L"Failed to get memory map: %r\n", status);
+      Print(L"failed to get memory map: %r\n", status);
       while (1);
     }
     status = gBS->ExitBootServices(image_handle, memmap.map_key);
     if (EFI_ERROR(status)) {
-      Print(L"Could not exit bott service: %r\n", status);
+      Print(L"Could not exit boot service: %r\n", status);
+      while (1);
     }
   }
 
   // call_kernel
   UINT64 entry_addr = *(UINT64*) (kernel_base_addr + 24);
 
-  typedef void EntryPointType(void);
+  typedef void EntryPointType(UINT64, UINT64);
   EntryPointType* entry_point = (EntryPointType*) entry_addr;
-  entry_point();
+  entry_point(gop->Mode->FrameBufferBase, gop->Mode->FrameBufferSize);
 
 
   Print(L"All done\n");
